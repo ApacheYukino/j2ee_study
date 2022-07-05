c5e1d37a5c806c71ff74f3266f18474f73660a10
yukino <1826544435@qq.com>
2022-07-05 16:01:37 +0800

    项目初始化

---
+++ src/main/java/com/yukino/j2ee_study/J2eeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.yukino.j2ee_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2eeStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yukino/j2ee_study/J2eeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukino.j2ee_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
